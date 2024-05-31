d5d0b6eb8ec81d4ea1934afd6bfcd86a56b3fabc
XarrrdaS <k.radomski1226@gmail.com>
2024-05-31 14:08:48 +0200

    base - validation

--- graph_generator/main.py
+++ graph_generator/main.py
@@ -1,14 +1,17 @@
 def main():
-    mode = input(">>> ")
+    while True:
+        mode = input(">>> ")
 
-    if mode == '--hamilton':
-        n = int(input("nodes> "))
-        saturation = int(input("saturation> "))
-    elif mode == '--non-hamilton':
-        n = int(input("nodes> "))
-    else:
-        print("Invalid argument. Use --hamilton or --non-hamilton")
-        return
+        if mode == '--hamilton':
+            n = int(input("nodes> "))
+            saturation = int(input("saturation> "))
+            return True
+        elif mode == '--non-hamilton':
+            n = int(input("nodes> "))
+            return True
+        else:
+            print("Invalid argument. Use --hamilton or --non-hamilton\n")
+            continue
 
 if __name__ == "__main__":
     main()
