c1e6a8a69581e789b71a672a0f1b869bc2d30a2a
XarrrdaS <k.radomski1226@gmail.com>
2024-05-31 18:12:11 +0200

    menu with options + input adjustment

--- main.py
+++ main.py
@@ -3,60 +3,67 @@ from operations.generateGraph.non_hamiltonianGraph import generate_non_hamiltoni
 from operations.findCycle.euler import find_euler_cycle
 from operations.findCycle.hamilton import find_hamilton_cycle
 from operations.exportTikz.exportToTikz import export_to_tikz
-from operations.graph import Graph
 
 def main():
-    graph = None
-
     while True:
-        mode = input(">>> ")
+        while True:
+            mode = input(">>> ")
 
-        if mode == '--hamilton':
-            n = int(input("nodes> "))
-            saturation = int(input("saturation> "))
-            graph = generate_hamiltonian_graph(n, saturation)
-            break
-        elif mode == '--non-hamilton':
-            n = int(input("nodes> "))
-            graph = generate_non_hamiltonian_graph(n)
-            break
-        else:
-            print("Invalid argument. Use --hamilton or --non-hamilton\n")
-
-    print("\nAvailable actions:")
-    print("-d, --display")
-    print("       Display graph in the adjacency list\n")
-    print("-t, --tikz")
-    print("       Export graph to TikZ\n")
-    print("-e, --euler")
-    print("       Find Eulerian cycle\n")
-    print("-h, --hamilton")
-    print("       Find Hamiltonian cycle\n")
-    print("-x, --exit")
-    print("       Exit the program\n")
-    
-    while True:
-        action = input(">>> ")
-
-        if action in ('--display', '-d'):
-            print("\nGenerated graph in the adjacency list:")
-            graph.display()
-        elif action in ('--tikz', '-t'):
-            export_to_tikz(graph)
-        elif action in ('--euler', '-e'):
-            print("\nEulerian Cycle:")
-            euler_cycle = find_euler_cycle(graph.copy())
-            if euler_cycle:
-                print(euler_cycle)
-        elif action in ('--hamilton', '-h'):
-            print("\nHamiltonian Cycle:")
-            hamilton_cycle = find_hamilton_cycle(graph.copy())
-            if hamilton_cycle:
-                print(hamilton_cycle)
+            if mode == '--hamilton':
+                n = int(input("nodes> "))
+                saturation = int(input("saturation> "))
+                graph = generate_hamiltonian_graph(n, saturation)
+                break
+            elif mode == '--non-hamilton':
+                n = int(input("nodes> "))
+                graph = generate_non_hamiltonian_graph(n)
+                break
             else:
-                print("Graph is not Hamiltonian")
-        elif action in ('--exit', '-x'):
-            break
+                print("Invalid argument. Use --hamilton or --non-hamilton\n")
+
+        options_menu = """\nAvailable actions:\n
+        -d, --display
+               Display graph in the adjacency list
+        -t, --tikz
+               Export graph to TikZ
+        -e, --euler
+               Check if there is Eulerian cycle
+        -h, --hamilton
+               Check if there is Hamiltonian cycle
+        -H, --help
+               Display help
+        -b, --back
+               Generate graph again
+        -x, --exit
+               Exit the program"""
+        
+        print(options_menu)
+        while True:
+            action = input(">>> ")
+
+            if action in ('--display', '-d'):
+                print("\nGenerated graph in the adjacency list:")
+                graph.display()
+            elif action in ('--tikz', '-t'):
+                export_to_tikz(graph)
+            elif action in ('--euler', '-e'):
+                print("\nEulerian Cycle:")
+                euler_cycle = find_euler_cycle(graph.copy())
+                if euler_cycle:
+                    print(euler_cycle)
+            elif action in ('--hamilton', '-h'):
+                print("\nHamiltonian Cycle:")
+                hamilton_cycle = find_hamilton_cycle(graph.copy())
+                if hamilton_cycle:
+                    print(hamilton_cycle)
+                else:
+                    print("Graph is not Hamiltonian")
+            elif action in ('--help', '-H'):
+                print(options_menu)
+            elif action in ('--back', '-b'):
+                break
+            elif action in ('--exit', '-x'):
+                return
 
 if __name__ == "__main__":
     main()
